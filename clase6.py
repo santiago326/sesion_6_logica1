22922178d0e3702f714ddd0ada0052dc50458901
santiago326 <tapiasms24@gmail.com>
2025-06-19 19:47:17 -0500

    Update clase6.py

--- clase6.py
+++ clase6.py
@@ -1,1 +1,3 @@
-print("hola mundo")
+# print("hola mundo")
+# #implementar una calculadora que pida dos numeros y una operacion, vamos a usar la figura se switch...casey luego con if...elif...else.
+#calculadora basica:
