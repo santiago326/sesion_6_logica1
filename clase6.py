c5822bbeffc5fc36f38b32949c36423a4b523afb
santiago326 <tapiasms24@gmail.com>
2025-06-19 20:18:51 -0500

    ejjjj

--- clase6.py
+++ clase6.py
@@ -3,25 +3,39 @@
 #calculadora basica:
 
 
-num_1 =float(input("primer numero a consulta: "))
-num_2 =float(input("segundo numero a consultar: "))
-operador =input("que operacion vas hacer?(+,-,/,*): ")
+# num_1 =float(input("primer numero a consulta: "))
+# num_2 =float(input("segundo numero a consultar: "))
+# operador =input("que operacion vas hacer?(+,-,/,*): ")
 
-#como asi que switch y case?
-#switch y case es una forma diferente de escribir condicionales,donde se evaluan un parametro,booleano0, y se va a direccionar inmediatamente segun los casos definidos
+# #como asi que switch y case?
+# #switch y case es una forma diferente de escribir condicionales,donde se evaluan un parametro,booleano0, y se va a direccionar inmediatamente segun los casos definidos
 
-match operador:
-    case "+":
-        print("resultado", num_1 + num_2)
-    case "-":
-        print("resultado", num_1 - num_2)
-    case "*":
-        print("resiltado", num_1 * num_2)
-    case "/":
-        if num_2 != 0:
-            print("resultado", num_1 / num_2)
-        else:
-            print("resultado no valido") 
-    case _:
-        print("resultado no valido")
+# match operador:
+#     case "+":
+#         print("resultado", num_1 + num_2)
+#     case "-":
+#         print("resultado", num_1 - num_2)
+#     case "*":
+#         print("resiltado", num_1 * num_2)
+#     case "/":
+#         if num_2 != 0:
+#             print("resultado", num_1 / num_2)
+#         else:
+#             print("resultado no valido") 
+#     case _:
+#         print("resultado no valido")
 
+if operador == "+":
+    print("resultado", num_1 + num_2)
+elif operador == "-":
+    print("resultado", num_1 - num_2)
+elif operador == "*":
+    print("resultado", num_1 * num_2)
+elif operador == "/":
+    if num_2 != 0:
+        print("resultado", num_1 / num_2)
+    else: 
+        print("erros dividiendo por cero.")
+else:
+    print("operacion no valida.")
+    
